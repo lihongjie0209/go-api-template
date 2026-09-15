77412da332e621f2a03bdcffc4e9e5466ff125fb
李宏杰 <lihongjie0209@gmail.com>
2026-09-16 02:51:10 +0800

    fix: migrate mysql text fields compatibly

--- migrations/mysql/000016_file_object_deletion_retry.up.sql
+++ migrations/mysql/000016_file_object_deletion_retry.up.sql
@@ -1,8 +1,12 @@
 ALTER TABLE files
     ADD COLUMN object_deleted_at timestamp(6) NULL,
     ADD COLUMN object_delete_attempts bigint NOT NULL DEFAULT 0,
-    ADD COLUMN object_delete_error text NOT NULL DEFAULT '',
+    ADD COLUMN object_delete_error text NULL,
     ADD COLUMN object_delete_next_at timestamp(6) NULL;
+
+UPDATE files SET object_delete_error = '' WHERE object_delete_error IS NULL;
+
+ALTER TABLE files MODIFY COLUMN object_delete_error text NOT NULL;
 
 CREATE INDEX files_object_delete_pending_idx
     ON files (object_delete_next_at, id(128));
