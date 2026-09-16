21c9e18156f315383a80ca07f27722e748df280a
李宏杰 <lihongjie0209@gmail.com>
2026-09-16 16:03:14 +0800

    close operation and security log audit

--- internal/routepolicy/service_test.go
+++ internal/routepolicy/service_test.go
@@ -82,7 +82,7 @@ func TestBoundedRouteAuditNamePreservesUTF8(t *testing.T) {
 	got := boundedRouteAuditName(value)
 	require.LessOrEqual(t, len(got), maxRouteAuditNameBytes)
 	require.True(t, utf8.ValidString(got))
-	require.Equal(t, value[:100], boundedRouteAuditName(value[:100]))
+	require.Equal(t, "http POST /short", boundedRouteAuditName("http POST /short"))
 }
 
 func TestSetRollsBackWhenTransactionalAuditFails(t *testing.T) {
